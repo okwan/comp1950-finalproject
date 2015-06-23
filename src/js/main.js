2df9798dd3fdafa806a6dcaf32c0495089c15584
Oscar Kwan <oscar.kwan@globalrelay.net>
2015-06-23 13:20:04 -0700

    exercise button updated

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,28 @@
 $navOverlayTrigger = $(".nav-overlay-trigger");
 $navOverlay = $(".nav-overlay");
 $navOverlayTriggerClose = $(".nav-overlay-trigger-close");
+$body = $("body");
+$floatingButton = $(".button");
 
+$navLectures = $(".nav-lectures");
+$navExercises = $(".nav-exercises");
 
 // navigation toggle add & remove
 $($navOverlayTrigger).click(function() {
-	$navOverlay.addClass('nav-open');
+	$navLectures.addClass('nav-open');
+	$body.css('overflow', 'hidden');
 });
 
 $($navOverlayTriggerClose).click(function() {
-	$navOverlay.removeClass('nav-open');
+	$navLectures.removeClass('nav-open');
+	$navExercises.removeClass('nav-open');
+	$body.css('overflow', 'auto');
 });
 
+$($floatingButton).click(function() {
+	$navExercises.addClass('nav-open');
+	$body.css('overflow', 'hidden');
+});
 
 
 
